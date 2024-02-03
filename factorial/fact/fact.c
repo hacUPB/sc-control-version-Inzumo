44a53590e8c469778a82023c246e5972b49e4b43
Inzumo <146497015+Inzumo@users.noreply.github.com>
2024-02-03 20:53:16 +0000

    número primo

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -30,7 +30,26 @@ int main(int argc, char *argv[]) {
     
     /*Implement the function that calculates if the argument is a prime number*/
 
+ int count = 0,index = 1;
+    while (index < number)
+    {
+        if(number%index == 0)
+            {
+            count++;
+            }
+        index++;
+    }
+
     /*Print the result*/
     
+    if(count == 1)
+    {
+        printf("%d es número primo\n",number);
+    }
+    else
+    {
+        printf("%d no es número primo\n",number);
+    }
+
     return 0;
 }
