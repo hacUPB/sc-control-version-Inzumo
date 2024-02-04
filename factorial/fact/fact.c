1a9db3dd82fd738084acc58f592f07e5d132b026
Inzumo <146497015+Inzumo@users.noreply.github.com>
2024-02-04 14:48:17 +0000

    Agrega código para calcular factorial y verificar si es un número primo

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -16,9 +16,35 @@ int main(int argc, char *argv[]) {
         retun(1);
     }
     /*Implement the function that calculates the factorial of the input argument*/
-    
+
+        int factorial = 1;
+    for (int i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+
     /*Implement the function that calculates if the argument is a prime number*/
 
+int isPrime = 1;
+    if (number <= 1) {
+        isPrime = 0;
+    } else {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
     /*Print the result*/
+
+    printf("Factorial of %d: %d\n", number, factorial);
+    if (isPrime) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
     
 }
