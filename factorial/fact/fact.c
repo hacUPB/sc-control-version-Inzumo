54e333c77f5990b6af45dc71a73a511874f7bafb
Inzumo <146497015+Inzumo@users.noreply.github.com>
2024-02-05 21:37:52 +0000

    Agrega código para calcular factorial y verificar si es un número primo

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char argv[]) {
-    if (argc < 2) {
-        fprintf(stderr,"Usage: program <number>\n");
+long long factorial(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int is_prime(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    int number;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: program <number>\n");
+        return 1;
+    } else {
+        number = atoi(argv[1]); //Convert the console input argument to an integer
+    }
+
+    /*It must be verified that the entered number is greater than or equal to zero*/
+    if (number < 0) {
+        fprintf(stderr, "The number must be greater than or equal to zero\n");
         return 1;
     }
 
-    int number = atoi(argv[1]); //Convert the console input argument to an integer
+    /*Implement the function that calculates the factorial of the input argument*/
+    long long fact = factorial(number);
 
-    /It must be verified that the entered number is greater than or equal to zero/
-    if(number < 0)
-    {
-        fprintf(stderr,"The number must be greater than or equal to zero\n");
-        return(1);
-    }
-    /Implement the function that calculates the factorial of the input argument/
-        int factorial = 1;
-    for (int i = 1; i <= number; i++) {
-        factorial= i;
-    }
+    /*Print the result*/
+    printf("The factorial of %d is: %lld\n", number, fact);
 
-    /Implement the function that calculates if the argument is a prime number/
-int isPrime = 1;
-    if (number <= 1) {
-        isPrime = 0;
+    /*Implement the function that calculates if the argument is a prime number*/
+    int prime = is_prime(number);
+
+    /*Print the result*/
+    if (prime == 1) {
+        printf("%d is a prime number\n", number);
     } else {
-        for (int i = 2; i <= number / 2; i++) {
-            if (number % i == 0) {
-                isPrime = 0;
-                break;
-            }
-        }
-    }
-
-    /Print the result/
-
-    printf("Factorial of %d: %d\n", number, factorial);
-    if (isPrime) {
-        printf("%d is a prime number.\n", number);
-    } else {
-        printf("%d is not a prime number.\n", number);
+        printf("%d is not a prime number\n", number);
     }
 
     return 0;
-
 }
